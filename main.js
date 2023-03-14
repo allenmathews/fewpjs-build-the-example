1a8b631aaf2e256dae9079b458421eba6793bad4
Allen Mathews <allenjoemathews@gmail.com>
2023-03-15 09:44:33 +1100

    commit tree 35a3458860da48cbb8c9e1e435ec74ae07e1db5e

--- main.js
+++ main.js
@@ -4,22 +4,45 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.getElementById('modal').className = 'hidden'
+const hearts = document.getElementsByClassName('like-glyph')
 
+for (let i = 0; i <= hearts.length - 1; i++) {
+    hearts[i].addEventListener('click', () => {
+        mimicServerCall()
+            .then(function() {
+                // if (hearts[i].textContent === EMPTY_HEART) {
+                //     console.log(hearts[i].textContent === EMPTY_HEART)
+                //     hearts[i].textContent = FULL_HEART
+                //     hearts[i].classList.add('activated-heart')
+                if (hearts[i].textContent === EMPTY_HEART) {
+                    console.log(hearts[i].textContent === EMPTY_HEART)
 
-
+                } else {
+                    console.log(hearts[i].textContent === EMPTY_HEART)
+                    hearts[i].textContent = EMPTY_HEART
+                    hearts[i].classList.remove('activated-heart')
+                }
+            }).catch(() => {
+                document.getElementById('modal').classList.remove('hidden')
+                setTimeout(() => document.getElementById('modal').className = 'hidden', 1000)
+            })
+    })
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
-  });
+function
+mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            let isRandomFailure = Math.random() < .2
+            if (isRandomFailure) {
+                reject("Random server error. Try again.");
+            } else {
+                resolve("Pretend remote server notified of action!");
+            }
+        }, 300);
+    });
 }
